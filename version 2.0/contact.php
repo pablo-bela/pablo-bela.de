f981b0eb9c41d54cee20c0a26911c0c2accc1d84
Royal_Miner <81974635+RoyalMiner@users.noreply.github.com>
2023-05-18 20:59:56 +0200

    Add files via upload

---
+++ version 2.0/contact.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../index.css">
+    <title>Kontakt</title>
+    <link rel="shortcut icon" href="../Bilder/crown-2-svgrepo-com.svg" type="image/x-icon">
+</head>
+<body>
+
+</body>
+</html>
