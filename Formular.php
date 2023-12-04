81fe194a87fa7ddf56622f667f05b89ed6a70a89
royal_miner <1899pablo@gmail.com>
2023-12-04 18:35:08 +0100

    4.12.2023 - 2

--- Formular.php
+++ Formular.php
@@ -3,10 +3,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Formulardaten abrufen
     $vorname = $_POST["vorname"];
     $nachname = $_POST["nachname"];
+    $email = $_POST["email"];
     $anliegen = $_POST["anliegen"];
 
     // E-Mail-Empfänger
-    $empfaenger = "1899pablo@gmail.com";
+    $empfaenger = "deine@email.com";
 
     // E-Mail-Betreff
     $betreff = "Neue Nachricht von $vorname $nachname";
@@ -14,10 +15,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // E-Mail-Nachricht
     $nachricht = "Vorname: $vorname\n";
     $nachricht .= "Nachname: $nachname\n";
+    $nachricht .= "E-Mail: $email\n";
     $nachricht .= "Anliegen:\n$anliegen";
 
     // E-Mail-Header
-    $header = "From: $vorname $nachname <$empfaenger>";
+    $header = "From: $vorname $nachname <$email>";
 
     // E-Mail senden
     mail($empfaenger, $betreff, $nachricht, $header);
